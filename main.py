e4138752e5074d374eb84af1d31b4d935a1bec9f
Aloosh101 <101348624+Aloosh101@users.noreply.github.com>
2023-02-03 23:43:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
 os.system("/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip")
 os.system("pip3 install -r requirements.txt")
+os.system("pip3 install Django==4.1.3")
 os.system("python manage.py runserver")
