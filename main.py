8b7a314b6dba0d2be22b349e92883014c581753d
Aloosh101 <101348624+Aloosh101@users.noreply.github.com>
2023-02-04 01:12:09 +0300

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system("/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip")
-os.system("pip3 install -r requirements.txt")
-os.system("pip3 install Django==4.1.3")
-os.system("python manage.py runserver")
